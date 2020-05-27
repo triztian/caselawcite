e25a86e7df51db6d826947f06d5fd7407b79c5c4
Tristian Azuara <tristian.azuara@gmail.com>
2020-05-26 23:43:00 -0700

    Add Notebook for processing attorney names

---
+++ ETL/attorney_names.py
@@ -0,0 +1,81 @@
+
+from string_processing import *
+
+known_titles = {
+    'attorney',
+    'solicitor',
+    'assistant',
+    'general',
+    'public',
+    'guardian'
+}
+
+known_case_parts = {
+    'appellant',
+    'appellee', 
+    'plaintiff',
+    'defendant'
+}
+
+non_name_words = {
+    'llc',
+    'ltd.',
+    'inc.',
+    'argued',
+    'all of',
+    'p.c.',
+    '&'
+}
+
+def is_title(string):
+    """Determines if the string is a position title."""
+    return bool(set(string.split(' ')) & known_titles)
+
+
+def is_place_of_origin(string):
+    """Determines if the string is a location or non-person."""
+    return string.startswith('of ') or string.startswith('both ')
+
+
+def is_party_indicator(string):
+    """Determines if the string indicates the party to which the attorney 
+    belongs to.
+    """
+    return string.startswith('for ')
+
+
+def is_other_string(string):
+    """Indicates that the string represents some other entity or meaning."""
+    return bool(set(string.split(' ')) & non_name_words)
+
+
+def is_person_name(string):
+    """Determines if the string is a person name or not."""
+    return all([
+        not is_title(string), 
+        not is_other_string(string), 
+        not is_place_of_origin(string),
+        not is_party_indicator(string)
+    ])
+
+
+def fix_jr_suffixes(name_strings):
+    """Fixes 'jr.' suffixes by combining them with the previous name"""
+    prev = None
+    for name_string in name_strings:
+        current = name_string
+        if current == "jr.":
+            current = prev + ' jr.'
+
+        yield current 
+        prev = name_string
+
+
+def parse_attorney_names(case_attorneys_items):
+    clean_parts = [split_clean(item) for item in case_attorneys_items]
+
+    name_strings = flatten(clean_parts)
+
+    name_strings = fix_jr_suffixes(name_strings)
+
+    return [string for string in name_strings if is_person_name(string)]
---
+++ ETL/string_processing.py
@@ -0,0 +1,31 @@
+
+def flatten(l):
+	"""Flattens an array of arrays"""
+    result = []
+    for sublist in l:
+        for item in sublist:
+            result.append(item)
+    return result 
+
+
+def clean_chars(input):
+	"""Remove 'special' non name characters"""
+    return input \
+        .replace('(', '') \
+        .replace(')', '') \
+        .replace('and ', '') \
+        .strip()
+
+
+def split_clean(input_item):
+	"""Splits each raw attorney item and cleans odd characters like parens, 
+	ampersands, commas, etc and returns a single array with string names.
+	"""
+    parts = input_item.split(',')
+    parts = flatten([p.split('(') for p in parts])
+    parts = flatten([p.split(')') for p in parts])
+
+    parts = [p.lower() for p in parts]
+    parts = [clean_chars(p) for p in parts]
+    parts = [p for p in parts if bool(p)]
+    return parts
