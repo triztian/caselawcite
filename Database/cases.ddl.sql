f820dd0cce4cf5bd4d8e2a856eadfa4a7a038584
Tristian Azuara <tristian.azuara@gmail.com>
2020-05-28 01:45:40 -0700

    Add case docket numbers table

--- Database/cases.ddl.sql
+++ Database/cases.ddl.sql
@@ -1,13 +1,19 @@
 CREATE TABLE IF NOT EXISTS cases (
-	id PRIMARY KEY INTEGER,
-	docket_number TEXT NOT NULL,
-	jurisdiction TEXT NOT NULL,
-	court TEXT NOT NULL,
-	volume INTEGER NOT NULL,
-	first_page INTEGER NOT NULL,
-	last_page INTEGER NOT NULL,
-	decision_date DATE NOT NULL,
+	id 				INTEGER PRIMARY KEY,
+	-- docket_number 	INTEGER NOT NULL,
+	jurisdiction 	TEXT NOT NULL,
+	court 			TEXT NOT NULL,
+	volume 			INTEGER NOT NULL,
+	first_page 		INTEGER NOT NULL,
+	last_page 		INTEGER NOT NULL,
+	decision_date 	DATE NOT NULL,
 	url TEXT
+);
+
+CREATE TABLE IF NOT EXISTS case_docket_numbers (
+	case_id 		INTEGER NOT NULL,
+	docket_number 	INTEGER NOT NULL,
+	PRIMARY KEY(case_id, docket_number)
 );
 
 CREATE TABLE IF NOT EXISTS case_citations (
