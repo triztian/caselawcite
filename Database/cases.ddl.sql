5bc536e2d48b742a41c0a9f46da1f3c65ef62809
Tristian Azuara <tristian.azuara@gmail.com>
2020-05-31 01:41:38 -0700

    Latest

--- Database/cases.ddl.sql
+++ Database/cases.ddl.sql
@@ -10,13 +10,13 @@ CREATE TABLE IF NOT EXISTS cases (
 	url TEXT
 );
 
+CREATE TABLE IF NOT EXISTS case_citations (
+	case_id 			INTEGER NOT NULL,
+	cites_to_case_id	INTEGER NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS case_docket_numbers (
 	case_id 		INTEGER NOT NULL,
 	docket_number 	INTEGER NOT NULL,
 	PRIMARY KEY(case_id, docket_number)
-);
-
-CREATE TABLE IF NOT EXISTS case_citations (
-	case_id 			INTEGER NOT NULL,
-	cites_to_case_id	INTEGER NOT NULL
 );
